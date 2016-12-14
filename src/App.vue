fb589ab22380904d2d2e0e5eefcd629d7f3741bc
korakodchatngoen <kiuytr335@hotmail.com>
2016-12-14 20:02:50 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Navbar></Navbar>
+    <mains></mains>
+  </div>
+</template>
+
+<script>
+import Mains from './components/Mains'
+import Showplay from './components/Showplay'
+import Navbar from './components/Navbar'
+
+export default {
+  name: 'app',
+  components: {
+    Mains,
+    Showplay,
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/Mains.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="row">
+          <div class="col-xs-6 col-md-2">
+            <a href="#!" class="thumbnail" style="vertical-align: middle">
+              <img class="menu" src="http://cdn.mysitemyway.com/etc-mysitemyway/icons/legacy-previews/icons/glossy-black-icons-alphanumeric/070841-glossy-black-icon-alphanumeric-letter-c.png" width="120">
+            <h4 class="title">
+              C++++
+            </h4>
+          </a>
+          </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
